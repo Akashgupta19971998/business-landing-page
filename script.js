9d6c5c2a2d66fbaf0867dc69a20b9153e442d818
Akash Kumar Gupta <117676075+Akashgupta19971998@users.noreply.github.com>
2023-06-06 23:01:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function submitForm() {
+    let fUll_name = document.getElementById('full-name').value;
+    let last_name = document.getElementById('last-name').value;
+    let email = document.getElementById('email').value;
+    let Reason_contacting  = document.getElementById('Reason-contacting').value;
+    console.log(fUll_name, last_name, email,Reason_contacting);
+    if (fUll_name != null && last_name != null && email != null && Reason_contacting != false) {
+         alert("success")
+    } else {
+        alert("All fields are required!")
+    }
+}
